634d825b0804ed8714e7a0c5b7c065e95a0ba7da
SOLANKI MILAN JAGDISHBHAI <Solankimilanjagdishbhaislanki@gmail.com>
2026-05-25 15:01:17 +0530

    changed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -75,3 +75,106 @@ return res.status(200).json({
     })
 }
 }
+
+export const deleteProduct = async (req, res) => {
+
+    try{
+        
+        const {productId} = req.params;
+        const product = await Product.findById(productId);
+        if(!product){
+            return res.status(404).json({
+                success: false,
+                message: "Product not found"
+            })
+        }
+
+        // delete images from cloudinary
+
+       if(product.productImg && product.productImg.length > 0){
+        for(let img of product.productImg){
+           const result = await cloudinary.uploader.destroy(img.public_id);
+        }}
+        // delete product from DB
+        await Product.findByIdAndDelete(productId);
+        return res.status(200).json({
+            success: true,
+            message: "Product deleted successfully"
+        })
+
+    }catch(error){
+        return res.status(500).json({
+        success: false,        
+        message: "Error deleting product",
+        error: error.message
+    })
+    }
+}
+
+export const updateProduct = async (req, res) => {
+    try{
+
+         const {productId} = req.params;
+        const {productName, productDesc, productPrice, category, brand, existingImages} = req.body;
+        const product = await Product.findById(productId);
+        if(!product){
+            return res.status(404).json({
+                success: false,
+                message: "Product not found"
+            })
+        }
+
+        let updatedImages = [];
+
+        // keep existing images that user wants to keep
+        if(existingImages ){
+            const keepIds = JSON.parse(existingImages);
+            updatedImages = product.productImg.filter((img) => keepIds.includes(img.public_id));
+
+            //delte only removed images 
+            const removedImages = product.productImg.filter((img) => !keepIds.includes(img.public_id));
+            for(let img of removedImages){
+                await cloudinary.uploader.destroy(img.public_id);
+            }
+        }else{
+            
+            updatedImages = product.productImg; // keep all existing images if no existingImages field is provided
+        }
+
+
+        // add new images if any
+        if(req.files && req.files.length > 0){
+            for (let file of req.files){
+                const fileuri = getdataUri(file);
+                const result = await cloudinary.uploader.upload(fileuri, {folder: "products"});
+                updatedImages.push({
+                    url: result.secure_url,
+                    public_id: result.public_id
+                });
+            }
+        }
+
+        // Update product 
+
+        product.productName = productName || product.productName;
+        product.productDesc = productDesc || product.productDesc;
+        product.productPrice = productPrice || product.productPrice;
+        product.category = category || product.category;
+        product.brand = brand || product.brand;
+        product.productImg = updatedImages;
+
+        await product.save();
+        return res.status(200).json({
+            success: true,
+            message: "Product updated successfully",
+            product: product
+        })
+
+    }catch(error){
+        return res.status(500).json({
+        success: false,        
+        message: "Error updating product",
+        error: error.message
+    })
+    }
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addProduct, getAllProduct } from "../controllers/productController.js";
+import { addProduct, getAllProduct, deleteProduct, updateProduct } from "../controllers/productController.js";
 import { isAdmin, isauthenticated } from "../middleware/isAuthonticated.js";
 import { multipleUpload } from "../middleware/multer.js";
 
@@ -8,5 +8,6 @@ const router = express.Router();
 
 router.post("/add", isauthenticated, isAdmin, multipleUpload, addProduct);
 router.get("/get-all", getAllProduct);
-
+router.delete("/delete/:productId", isauthenticated, isAdmin, deleteProduct);
+router.put("/update/:productId", isauthenticated, isAdmin, multipleUpload, updateProduct);
 export default router;
