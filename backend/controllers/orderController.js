6af13e21e8487b8f70527ad464c8353c8b1b398b
SOLANKI MILAN JAGDISHBHAI <Solankimilanjagdishbhaislanki@gmail.com>
2026-07-31 12:54:56 +0530

    dashboard add

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,10 @@
 import razorpayInstance from '../config/razorpay.js';
 import { Order}  from '../models/orderModel.js';
+import {User} from '../models/userModel.js';
+import {Product} from '../models/productModel.js'
 import crypto from 'crypto';
 import { Cart } from '../models/cartModel.js';
+import { Aggregate } from 'mongoose';
 
 export const createOrder = async (req, res) => {
     try {
@@ -56,9 +59,105 @@ export const verifyPayment = async (req, res) => {
             res.status(200).json({success:true, message: "Payment verified successfully", order: order });
         } else {
             await Order.findOneAndUpdate({ razorpayOrderId: razorpay_order_id }, { status: "Failed" }, { new: true });
-            res.status(400).json({ message: "Invalid payment signature" });
+            res.status(400).json({ success:false, message: "Invalid payment signature" });
         }
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({ success:false, message: error.message });
+    }
+}
+
+export const getMyOrder = async (req, res) => {
+    try {
+        const userId = req.user._id
+        const orders = await Order.find({user:userId})
+        .populate({path:"products.productId", select:"productName productPrice productImg"})
+        .populate("user", "firstName lastName email")
+
+        res.status(200).json({success:true, count:orders.length, orders,})
+    } catch (error) {
+        console.error("Error fetching user Orders", error)
+        res.status(500).json({success:false, message:"error geting orders"})
+    }
+}
+
+export const getUserOrders = async (req, res) => {
+    try {
+        const {userId} = req.params
+        const orders = await Order.find({user:userId})
+        .populate({
+            path:"products.productId",
+            select:"productName productPrice productImg"
+        })
+        .populate("user","firstName lastName email")
+
+        res.status(200).json({success:true, count:orders.length, orders})
+    } catch (error) {
+        console.log("No such User found", error)
+        res.status(500).json({success:false, message:"error getting Users Orders details"})
+    }
+}
+
+export const getAllOrdersAdmin = async (req, res) => {
+    try {
+        const orders = await Order.find()
+        .sort({createdAt: -1})
+        .populate("user","firstName lastName email")
+        .populate("products.productId", "productName productPrice");
+
+        console.log(JSON.stringify(orders, null, 2));
+
+        res.status(200).json({success:true, count:orders.length, orders})
+    } catch (error) {
+        console.log("error fetching orders for admin", error)
+        res.status(500).json({success:false, message: "error fetching user's data for admin"})
+    }
+}
+
+export const getSalesData = async (req, res) => {
+    try {
+        const totalUsers = await User.countDocuments({})
+        const totalProducts = await Product.countDocuments({})
+        const totalOrders = await Order.countDocuments({status:"Paid"})
+
+        //total sales amount
+        const totalSaleAgg = await Order.aggregate([
+            {$match:{status:"Paid"}},
+            {$group:{_id:null, total:{$sum:"$amount"}}}
+        ])
+
+        const totalSales = totalSaleAgg[0]?.total || 0
+
+        //Sales grouped by date  (last 30 days)
+        const thirtyDaysAgo = new Date()
+        thirtyDaysAgo.setDate(thirtyDaysAgo.getDate()-30)
+
+        const salesByDate = await Order.aggregate([
+            {$match:{status:"Paid", createdAt:{$gte:thirtyDaysAgo}}},
+            {
+                $group:{
+                    _id:{
+                        $dateToString:{
+                            format:"%Y-%m-%d", date: "$createdAt"
+                        }
+                    }, 
+                    amount:{
+                        $sum:"$amount"
+                    },
+                }
+            },
+            {$sort:{_id:1}}
+        ])
+         console.log(salesByDate)
+        const formattedSales = salesByDate.map((item)=> ({
+            date:item._id,
+            amount:item.amount
+        }))
+         console.log(formattedSales)
+
+         res.status(200).json({success:true, message:"fetching sales data", totalOrders, totalProducts, totalUsers, totalSales, sales:formattedSales})
+         
+    } catch (error) {
+        console.error("error getting sales data", error)
+        res.status(500).json({success:false, message:"error getting Sales Data"})
     }
 }
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { createOrder, verifyPayment } from '../controllers/orderController.js';
-import {isauthenticated} from '../middleware/isAuthonticated.js';
+import { createOrder, getAllOrdersAdmin, getMyOrder, getSalesData, getUserOrders, verifyPayment } from '../controllers/orderController.js';
+import {isAdmin, isauthenticated} from '../middleware/isAuthonticated.js';
 
 const router = express.Router();
 
 router.post('/create-order', isauthenticated, createOrder);
 router.post('/verify-payment', isauthenticated, verifyPayment);
+router.get('/myorder', isauthenticated, getMyOrder);
+router.get('/all', isauthenticated, isAdmin, getAllOrdersAdmin);
+router.get('/user-order/:userId', isauthenticated, isAdmin, getUserOrders);
+router.get('/sales', isauthenticated, isAdmin, getSalesData);
+
 
 export default router;
