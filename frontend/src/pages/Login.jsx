7c8d8333b4dbbd75c72bc4c9fb2c2a2f404f1736
SOLANKI MILAN JAGDISHBHAI <Solankimilanjagdishbhaislanki@gmail.com>
2026-09-12 21:18:47 +0530

    A

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -40,11 +40,8 @@ const Login = () => {
 
     try {
       setLoading(true);
-      const res = await api.post(
-        "/user/login",
-        formData,
-        {},
-      );
+      const res = await api.post("/user/login", formData);
+      
       if (res.data.success) {
         toast.success(res.data.message);
 
