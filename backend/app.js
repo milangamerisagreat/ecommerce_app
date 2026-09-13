9b07081af138af1cd28c5693f7275f942f6aa890
SOLANKI MILAN JAGDISHBHAI <Solankimilanjagdishbhaislanki@gmail.com>
2026-09-13 09:56:09 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "http://localhost:5173",
   credentials: true,
   
 }));
