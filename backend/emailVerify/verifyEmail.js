03e6244773f086d546ef52036b278252675784de
SOLANKI MILAN JAGDISHBHAI <Solankimilanjagdishbhaislanki@gmail.com>
2026-09-13 22:48:51 +0530

    verify email text change

--- backend/emailVerify/verifyEmail.js
+++ backend/emailVerify/verifyEmail.js
@@ -19,7 +19,7 @@ export const sendVerificationEmail = (token, email) => {
     from: process.env.MAIL_USER,
     to: email,
     subject: "Email Verification",
-    text: `Please verify your email by clicking the following link: http://localhost:5173/verify-email/${token}`,
+    text: `Please verify your email by clicking the following link: ${frontendUrl}/verify-email/${token}`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
