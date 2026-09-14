0ed68b7df8e20a0bb9ce395f766191a1b9f4871a
SOLANKI MILAN JAGDISHBHAI <Solankimilanjagdishbhaislanki@gmail.com>
2026-09-14 11:47:50 +0530

    verify Mail auth change

--- backend/emailVerify/verifyEmail.js
+++ backend/emailVerify/verifyEmail.js
@@ -8,7 +8,9 @@ dotenv.config();
 
 export const sendVerificationEmail = (token, email) => {
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASS,
